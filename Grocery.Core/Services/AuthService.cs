3b54bbe188f72ea790065407e53e57a1b6e08bfe
Rohat2332 <rohataliyildirim@gmail.com>
2025-09-19 18:52:37 +0200

    Implemented login functionality(UC6)

--- Grocery.Core/Services/AuthService.cs
+++ Grocery.Core/Services/AuthService.cs
@@ -13,10 +13,12 @@ namespace Grocery.Core.Services
         }
         public Client? Login(string email, string password)
         {
-            //Vraag de klantgegevens [Client] op die je zoekt met het opgegeven emailadres
-            //Als je een klant gevonden hebt controleer dan of het password matcht --> PasswordHelper.VerifyPassword(password, passwordFromClient)
-            //Als alles klopt dan klantgegveens teruggeven, anders null
-            return null;
+            var client = _clientService.Get(email);
+            if (client == null) return null;
+
+            if (client.Password == password)
+            { return client; }
+            return null; 
         }
     }
 }
